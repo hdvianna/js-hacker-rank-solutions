ca1712e62913fbdd585ef1ce17399e1e5529b207
hdvianna <hdvianna@gmail.com>
2020-04-11 16:12:05 -0300

    removed duplicated code

--- src/medium/balanced-brackets/solution.js
+++ src/medium/balanced-brackets/solution.js
@@ -6,19 +6,16 @@ function BalancedBracket() {
             let len = string.length;
             if (len % 2 === 0) {
                 let stack = [];
-                if ((len % 2) === 0) {
-                    for (let i = 0; i < len; i++) {
-                        if (open.indexOf(string[i]) > -1) {
-                            stack.push(string[i]);
-                        } else if (stack.pop() !== closeDictionary[string[i]]) {
-                            return "NO";
-                        }
+                for (let i = 0; i < len; i++) {
+                    if (open.indexOf(string[i]) > -1) {
+                        stack.push(string[i]);
+                    } else if (stack.pop() !== closeDictionary[string[i]]) {
+                        return "NO";
                     }
                 }
                 return stack.length ? "NO" : "YES";
-            } else {
-                return "NO";
             }
+            return "NO";
         }
     }
 }
