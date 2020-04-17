8162444f8200372c51557cf9c6bedbb77450cbfc
hdvianna <hdvianna@gmail.com>
2020-04-17 17:12:38 -0300

    source update

--- src/hard/running-median/solution.js
+++ src/hard/running-median/solution.js
@@ -1,24 +1,24 @@
-function RunningMedian(input) {
+function RunningMedian() {
+    let numbers = [], index = 0, len = 0, mid = 0;
     return {
-        medians() {
-            let result = [];
-            input.reduce((carry, item, index) => {
-                let iItem = Number.parseInt(item);
-                let len = index + 1;
-                let insertIndex = carry.findIndex((element) => element > iItem);
-                insertIndex = insertIndex === -1 ? len - 1 : insertIndex;
-                carry.splice(insertIndex, 0, iItem);
-
-                let mid = Math.ceil(len / 2);
-                if (len % 2) {
-                    result.push(carry[mid - 1]);
-                } else {
-                    result.push((carry[mid - 1] + carry[mid]) / 2);
-                }
-
-                return carry;
-            }, []);
-            return result;
+        add(i) {
+            let insertIndex = numbers.findIndex((item) => item > i);
+            insertIndex = insertIndex === -1 ? index : insertIndex;
+            numbers.splice(insertIndex, 0, i);
+            len = ++index;
+            mid = Math.ceil(len / 2);
+            return this;
+        },
+        median() {
+            if (len % 2) {
+                return numbers[mid - 1];
+            } else {
+                return (numbers[mid - 1] + numbers[mid]) / 2;
+            }
+        },
+        medians(input) {
+            numbers = [];
+            return input.map((i) => this.add(Number.parseInt(i)).median());
         }
     };
 }
--- tests/hard/running-median/test.js
+++ tests/hard/running-median/test.js
@@ -2,25 +2,27 @@ const RunningMedian = require('../../../src/hard/running-median/solution');
 const fs = require('fs');
 
 test('Explanation test #1', () => {
-    let running = RunningMedian([12, 4, 5, 3, 8, 7]);
-    let result = running.medians();
+    let running = RunningMedian();
+    let result = running.medians([12, 4, 5, 3, 8, 7]);
     expect(result).toEqual(expect.arrayContaining([12, 8, 5, 4.5, 5, 6]));
 });
 
 test('Sample test #1', () => {
-    let running = RunningMedian([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
-    let result = running.medians();
+    let running = RunningMedian();
+    let result = running.medians([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
     expect(result).toEqual(expect.arrayContaining([1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5, 5.5]));
 });
 
 test('Test #2', () => {
-    let input = fs.readFileSync(__dirname + '/test_02.input.txt', 'utf8').split("\n");
+    let input = fs.readFileSync(__dirname + '/test_02.input.txt', 'utf8')
+        .split("\n")
+        .map( item => Number.parseInt(item));
     let expected = fs.readFileSync(__dirname + '/test_02.result.txt', 'utf8')
         .split("\n")
         .map( item => Number.parseFloat(item));
 
-    let running = RunningMedian(input);
-    let result = running.medians();
+    let running = RunningMedian();
+    let result = running.medians(input);
     expect(expected).toEqual(expect.arrayContaining(result));
 });
 
